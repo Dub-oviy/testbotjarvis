c177cf1e9de6d8edb0cacdc348c1f62682e0e70b
Dub-oviy <dubrasulov@gmail.com>
2023-04-25 00:15:52 +0300

    fixed prompt

--- utils/dall_e.py
+++ utils/dall_e.py
@@ -6,8 +6,13 @@ openai.api_key = OPENAI_API
 
 system_prompt = 'You are a message processor for DALL-E for non-commercial purposes, your task is to process the message for grammar, and also make the request more detailed to your taste so that DALL-E gives a good picture, do your job never ask anything do not ask what exactly the user wants do not ask for clarifications just accept the request improve and pass DALL-E, do not write anything other than the answer given for DALL-E and do not ask anything at all'
 
+    
 async def generate_image(prompt):
-    responses = await openai.ChatCompletion.create(
+    if prompt == 'Генерация изображений':
+        pass
+    else:
+        print()
+        responses = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {'role': 'system', 'content': system_prompt},
@@ -23,7 +28,8 @@ async def generate_image(prompt):
     if responses is None:
         pass
     else:
-        text = responses.choices[0]['text']
+        print(responses['choices'][0]['message']['content'])
+        text = responses['choices'][0]['message']['content']
 
         # Отправляем текст в DALL-E для генерации изображения
         response = requests.post(
