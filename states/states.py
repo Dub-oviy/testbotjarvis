558c6b332193962461e80f4208169f1beb089539
Dub-oviy <dubrasulov@gmail.com>
2023-04-25 23:15:19 +0300

    как же я люблю эту структуру

--- states/states.py
+++ states/states.py
@@ -1,7 +1,8 @@
+import aiogram
 from loader import dp,bot
 from aiogram import types
-from utils import chatgpt,dall_e,text_generate,translator_mode
-from keyboards.inline import inlinemarups
+from utils import chatgpt,dall_e,text_generate
+
 from keyboards.default import markups
 
 
@@ -13,6 +14,7 @@ async def chatgpt_handler(message: types.Message):
         await dp.current_state(user=message.from_user.id).reset_state()
         await message.answer('Вы вышли из режим Всезнайка',reply_markup=markups.mainMenu)
     else:
+        await bot.send_chat_action(message.chat.id, "typing")
         response = await chatgpt.get_chatgpt_message(message.text)
         await bot.send_message(chat_id=message.chat.id, text=response, reply_to_message_id=message.message_id)
 
@@ -26,17 +28,25 @@ async def dall_e_handler(message: types.Message):
         if message.text == 'Генерация изображений':
             pass
         else:
+            await message.answer( 'Интересный запрос , подождите немного и бот скинет изображение')
+            await bot.send_chat_action(message.chat.id, "upload_photo")
             response = await dall_e.generate_image(message.text)
+            bot.send_chat_action(message.chat.id, aiogram.types.ChatActions.TYPING)
             await bot.send_photo(chat_id=message.chat.id, photo=response, reply_to_message_id=message.message_id)
 
 async def text_generator_handler(message: types.Message):
 
     # await message.answer('Интересный запрос , подождите немного и бот скинет текст ')
     if message.text == 'Выход из режима':
-
         await dp.current_state(user=message.from_user.id).reset_state()
         await message.reply('Вы вышли из режим Генерация текста',reply_markup=markups.mainMenu)
     else:
-        response = await text_generate.text_generator(message.text)
-        await bot.send_message(chat_id=message.chat.id, text=response, reply_to_message_id=message.message_id)
+        if message.text == 'Генерация текста':
+            pass
+        else:
+            await message.answer('Интересный запрос , подождите немного и бот скинет текст ')
+            await bot.send_chat_action(message.chat.id, "typing")
+            response = await text_generate.text_generator(message.text)
+            await bot.send_chat_action(message.chat.id, 'typing')
+            await bot.send_message(chat_id=message.chat.id, text=response, reply_to_message_id=message.message_id)
 
--- utils/dall_e.py
+++ utils/dall_e.py
@@ -7,9 +7,30 @@ from data.config import OPENAI_API
 openai.api_key = OPENAI_API
 
 
+system_prompt = 'You are a message handler for DALL-E for non-commercial purposes, your task is to process the message for grammar, as well as make the request more detailed to your taste, so that DALL-E gives a good realistic colorful picture, do your job, never ask anything, do not ask what exactly the user wants to do, do not ask for clarification, just accept the request for improvement and pass DALL-E, do not write anything other than the answer given for DALL-E, and do not ask anything at all, if the user gave an insufficient number of details, give only FALSE and nothing more'
+
     
 async def generate_image(prompt):
     print(prompt)
+    responses = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {'role': 'system', 'content': system_prompt},
+            {'role': 'user', 'content': prompt},
+        ],
+        temperature=0.9,
+        max_tokens=3000,
+        top_p=1.0,
+        presence_penalty=0.6,
+        stop=["\n"],
+    )
+
+    if responses is None:
+            return None
+    else:
+            print(responses['choices'][0]['message']['content'])
+            text = responses['choices'][0]['message']['content']
+
         # Отправляем текст в DALL-E для генерации изображения
     response = requests.post(
         "https://api.openai.com/v1/images/generations",
@@ -18,7 +39,7 @@ async def generate_image(prompt):
             "Authorization": f"Bearer {openai.api_key}",
         },
         json={
-            "prompt": prompt,
+            "prompt": text,
             "num_images": 1,
             "size": "512x512",
             "response_format": "b64_json",
