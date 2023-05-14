7d86417b51ecbf6bd42e510905d01ef32c139dd7
Dub-oviy <dubrasulov@gmail.com>
2023-05-14 16:25:31 +0300

    add posters and changed env

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,4 +1,4 @@
-from loader import dp
+from loader import dp,bot,textphoto,translatephoto
 from aiogram import types
 from states.states import chatgpt_handler, dall_e_handler,translator_handler
 from keyboards.default import markups
@@ -15,16 +15,16 @@ async def bot_message(message: types.Message):
     if message.chat.type == 'private':
         # Проверяем текущее состояние пользователя
         state = dp.current_state(user=message.from_user.id)
-        if message.text == 'Генерация изображений':
+        if message.text == 'Генерация изображений🌄':
             # Если пользователь выбрал другой режим, сбрасываем текущее состояние на "dall_e"
             await state.set_state('dall_e')
-            await message.reply('Это режим генерации изображений, тут вы можете написать, что вы хотите увидеть, а потом бот сгенерирует вам изображение',
+            await bot.send_photo(message.chat.id , photo=textphoto ,caption ='Это режим генерации изображений, тут вы можете написать, что вы хотите увидеть, а потом бот сгенерирует вам изображение',
                                 reply_markup=markups.secondMenu)
             await dall_e_handler(message)
 
-        elif message.text == 'Режим переводчика':
+        elif message.text == 'Режим переводчика📚':
             await state.set_state('translator')
-            await message.reply('Вы вошли в режим переводчика, при написании текста на английском языке, бот автоматически переведет его на Русский.',
+            await bot.send_photo(message.chat.id , photo=translatephoto ,caption ='Вы вошли в режим переводчика, при написании текста на английском языке, бот автоматически переведет его на Русский.',
                                 reply_markup=markups.secondMenu)
             await translator_handler(message)
 
