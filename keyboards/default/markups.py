285d80ead0e046f3631432b755beab1091bcd7dc
Dub-oviy <dubrasulov@gmail.com>
2023-05-14 16:27:51 +0300

    fix spaces

--- keyboards/default/markups.py
+++ keyboards/default/markups.py
@@ -2,9 +2,9 @@ from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 
 
 
-Dall_e = KeyboardButton('Генерация изображений🌄')
-Trasnslator = KeyboardButton( 'Режим переводчика📚')
-Cansel = KeyboardButton("Выход из режима🔼")
+Dall_e = KeyboardButton('Генерация изображений 🌄')
+Trasnslator = KeyboardButton( 'Режим переводчика 📚')
+Cansel = KeyboardButton("Выход из режима 🔼")
 
 mainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
 mainMenu.add(Dall_e,Trasnslator)
--- states/states.py
+++ states/states.py
@@ -10,16 +10,16 @@ from keyboards.default import markups
 
 async def chatgpt_handler(message: types.Message):
     state = dp.current_state(user=message.from_user.id)
-    if message.text == 'Генерация изображений🌄' or message.text == 'Режим переводчика📚':
+    if message.text == 'Генерация изображений 🌄' or message.text == 'Режим переводчика📚':
         # Удаляем обработчик сообщений Всезнайка
         await dp.current_state(user=message.from_user.id).set_state(None)
         await message.answer('Вы вышли из режим Всезнайка',reply_markup=markups.mainMenu)
-        if message.text == 'Генерация изображений🌄':
+        if message.text == 'Генерация изображений 🌄':
             await dp.current_state(user=message.from_user.id).set_state('dall_e')
             await bot.send_photo(message.chat.id , photo=textphoto ,caption ='Это режим генерации изображений, тут вы можете написать, что вы хотите увидеть, а потом бот сгенерирует вам изображение',
                                 reply_markup=markups.secondMenu)
             await dall_e_handler(message)
-        elif message.text == 'Режим переводчика📚':
+        elif message.text == 'Режим переводчика 📚':
             await bot.send_photo(message.chat.id , photo=translatephoto ,caption ='Вы вошли в режим переводчика, при написании текста на английском языке, бот автоматически переведет его на Русский.',
                                 reply_markup=markups.secondMenu)
             await dp.current_state(user=message.from_user.id).set_state('translator')
@@ -31,12 +31,12 @@ async def chatgpt_handler(message: types.Message):
 
 async def dall_e_handler(message: types.Message):
     # await message.answer( 'Интересный запрос , подождите немного и бот скинет изображение')
-    if message.text == 'Выход из режима🔼':
+    if message.text == 'Выход из режима 🔼':
         # Удаляем обработчик сообщений Всезнайка
         await dp.current_state(user=message.from_user.id).reset_state()
         await message.reply('Вы вышли из режим Генерация изображений',reply_markup=markups.mainMenu)
     else:
-        if message.text == 'Генерация изображений🌄':
+        if message.text == 'Генерация изображений 🌄':
             pass
         else:
             await message.answer( 'Интересный запрос , подождите немного и бот скинет изображение')
@@ -46,11 +46,11 @@ async def dall_e_handler(message: types.Message):
             await bot.send_photo(chat_id=message.chat.id, photo=response, reply_to_message_id=message.message_id)
 
 async def translator_handler(message: types.Message):
-    if message.text == 'Выход из режима🔼':
+    if message.text == 'Выход из режима 🔼':
         await dp.current_state(user=message.from_user.id).reset_state()
         await message.reply('Вы вышли из режим переводчика',reply_markup=markups.mainMenu)
     else:
-        if message.text == 'Режим переводчика📚':
+        if message.text == 'Режим переводчика 📚':
             pass
         else:
             await bot.send_chat_action(message.chat.id, "typing")
