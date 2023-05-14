88acf26789cae1ee282646ba60810205c1f51f48
Rustamkhonov Mukhammad-Rakhimkhon <45013834+Mukhammad1999@users.noreply.github.com>
2023-05-14 14:22:37 +0500

    fix the int error

--- data/config.py
+++ data/config.py
@@ -2,13 +2,13 @@
 
 # # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 # env = Env()
-# env.read_env()
+# env.read_env() 
 
 # BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 # ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 # IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 # OPENAI_API = env.str("OPENAI_API")
 OPENAI_API = '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'
-ADMINS = 1133571937  # Тут у нас будет список из админов
+ADMINS = [1133571937]  # Тут у нас будет список из админов
 BOT_TOKEN = '6163546129:AAH66Ep47nFkQzolqsq-cD4xbvvew6saVg4'  # Забираем значение типа str
 
